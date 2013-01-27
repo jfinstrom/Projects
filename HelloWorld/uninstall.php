5488c0446ec2d6e5d390d009ee49668647e8943f
Moshe Brevda <mbrevda@gmail.com>
2013-01-27 17:09:43 +0200

    Update HelloWorld/uninstall.php
    Here's another way

--- HelloWorld/uninstall.php
+++ HelloWorld/uninstall.php
@@ -14,7 +14,7 @@ if (! function_exists("outn")) {
 }
 //Johnny Drop Tables
 out("Dropping all relevant tables");
-$sql = "DROP TABLE `HelloWorld_settings`";
-$result = $db->query($sql);
+sql("DROP TABLE `HelloWorld_settings`");
+
 
 ?>
